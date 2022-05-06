a4916111185deb2a6e69e5973d7cbab48618636b
Carson Weeks <CarsonWeeks@mail.com>
2022-05-06 14:49:25 -0700

    removed old files.

--- frontend/src/fetchFood.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const fetchFood = async (lat, long) => {
-    const response = await axios.get(`api/geofind/?lat=${lat}&long=${long}`)
-
-    return response.data
-}
-
-export default fetchFood
