82cc49197c9c367ca5d3ad28c39dca994c8e366b
Carson Weeks <CarsonWeeks@mail.com>
2022-05-06 12:43:53 -0700

    Scraping Rec when it cant find menu..

--- backend/geofinder/util/menuitem_models.py
+++ backend/geofinder/util/menuitem_models.py
@@ -50,20 +50,38 @@ def scrape_rec(response_html: str) -> list:
     return menu_items
 
 
+def attempt_to_scrape_rec(business: Business):
+    rec_response = requests.get(
+        f"https://www.yelp.com/biz/{business.slug}", allow_redirects=False
+    )
+    if rec_response.status_code == 200:
+        scraped_items = scrape_rec(rec_response.text)
+        if len(scraped_items) > 0:
+            for item in scraped_items:
+                create_menuitem_model(item, business)
+            business.method_for_query = "RC"
+        else:
+            business.method_for_query = "NA"
+    else:
+        business.method_for_query = "NA"
+    business.save()
+
+
 def get_menuitem_models(business: Business):
     if business.method_for_query == "RD":
+        business.method_for_query = "LD"
+        business.save()
         menu_response = requests.get(
             f"https://www.yelp.com/menu/{business.slug}/", allow_redirects=False
         )
         if menu_response.status_code == 200:
             scraped_items = scrape_menu(menu_response.text)
-            for item in scraped_items:
-                create_menuitem_model(item, business)
-        else:
-            rec_response = requests.get(
-                f"https://www.yelp.com/biz/{business.slug}", allow_redirects=False
-            )
-            if rec_response.status_code == 200:
-                scraped_items = scrape_rec(rec_response.text)
+            if len(scraped_items) > 0:
                 for item in scraped_items:
                     create_menuitem_model(item, business)
+                business.method_for_query = "MU"
+                business.save()
+            else:
+                attempt_to_scrape_rec(business)
+        else:
+            attempt_to_scrape_rec(business)
