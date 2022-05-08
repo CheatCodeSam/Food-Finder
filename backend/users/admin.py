ab478eca2ca28270d4ce217a55207af4393ba479
Carson Weeks <CarsonWeeks@mail.com>
2022-05-07 21:36:15 -0700

    Added customer user address.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,31 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import AbstractUser
+
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class UserAdminWithExtraFields(UserAdmin):
+    def __init__(self, *args, **kwargs):
+        super(UserAdminWithExtraFields, self).__init__(*args, **kwargs)
+
+        abstract_fields = [field.name for field in AbstractUser._meta.fields]
+        user_fields = [field.name for field in self.model._meta.fields]
+
+        self.fieldsets += (
+            (
+                "Extra fields",
+                {
+                    "fields": [
+                        f
+                        for f in user_fields
+                        if (f not in abstract_fields and f != self.model._meta.pk.name)
+                    ],
+                },
+            ),
+        )
+
+
+admin.site.register(User, UserAdminWithExtraFields)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    address = models.CharField(max_length=255, blank=True, null=True)
